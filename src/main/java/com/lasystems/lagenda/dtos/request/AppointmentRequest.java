bae9de11fe680d8b04b72698cb89e53a6b4758f9
Gledson Cruz <gledson.cruz@gmail.com>
2025-10-03 23:13:22 -0300

    Tratando next-available

--- src/main/java/com/lasystems/lagenda/dtos/request/AppointmentRequest.java
+++ src/main/java/com/lasystems/lagenda/dtos/request/AppointmentRequest.java
@@ -7,14 +7,14 @@ import java.time.ZoneId;
 import java.util.List;
 
 public record AppointmentRequest(
-        @NotNull(message = "ClientId é obrigatório")
+//        @NotNull(message = "ClientId é obrigatório")
         String clientId,
         @NotNull(message = "CompanyId é obrigatório")
         String companyId,
         LocalDateTime start,
         @NotNull(message = "ServiceId é obrigatório")
         List<String> serviceIds,
-        @NotNull(message = "SpecialistId é obrigatório")
+//        @NotNull(message = "SpecialistId é obrigatório")
         String specialistId,
         String providerId,
         ZoneId companyZoneId
